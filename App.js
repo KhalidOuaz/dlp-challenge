5a4769fcc14652167fc39aed5eb445dd36aa3a59
khalidouazari <dev.mkstudio@hotmail.fr>
2019-01-15 13:46:52 +0100

    Add Search Bar

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {SearchBar} from 'react-native-elements'
 
 export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {}
+  }
+
+  _onChangeText = async ( text ) => {
+    this.setState({searchText:text});
+    let movies = await ApiRequest.search(text);
+    stores.movies.dispatch({type:SET_SEARCH_MOVIES, payload:movies});
+    this.setState({movie:movies});
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <SearchBar
+            value={this.state.searchText}
+            containerStyle={{}}
+            showLoading
+            platform='ios'
+            cancelIcon={{ type: 'font-awesome', name: 'chevron-left' }}
+            placeholder='Search'
+            onChangeText={()=>this._onChangeText}
+        />
       </View>
     );
   }
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  }
 });
