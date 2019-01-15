c4f6267ed6f68262ed522fc51387c665f859cc76
Khalid Ouaz <dev.mkstudio@hotmail.com>
2019-01-15 14:01:23 +0100

    Add Request API

---
+++ src/js/controllers/ApiRequest.js
@@ -0,0 +1,19 @@
+import {Request} from "./Request";
+import {env} from "./../../env";
+
+export const API_URL = env.apiUrl;
+
+export const ApiRequest = class ApiRequest {
+
+    static async search(query) {
+        //console.log(API_URL + '/search'+ '?api_key=' + env.apiKeys + '&query=' + query);
+        return await Request.fetch(
+            API_URL + '/search/movie' + '?api_key=' + env.apiKeys + '&query='+ query, {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+            });
+    }
+};
